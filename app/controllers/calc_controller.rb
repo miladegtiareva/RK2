ee098463f2b5ae8da6d4082bf9923c99fa1796c2
Mila <miilladegtyarova@gmail.com>
2023-11-22 15:53:34 +0300

    rk2

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -8,40 +8,23 @@ class CalcController < ApplicationController
         if mas[0] != nil
           k = 0
           pr_0 = 0
-          prov = 0
+          prov3 = 0
           d = mas.length
           d.times do
-            if mas[k].to_i > 0
+            if mas[k].to_i > 0  
               pr_0 = 1
             end
-  
             k += 1
           end
           if pr_0 == 1
   
-              sum = 0
-              k = 0
-              index = -1
-              d.times do
-                if mas[k].to_i > 0
-                  sum += mas[k].to_i % 3
-                end
-                k += 1
-              end
-              k = 0
-              d.times do
-                if mas[k].to_i % 2 == 0
-                  index = k
-                end
-                k += 1
-              end
-              if index != -1
-                mas[index] = sum
-              end
-              @result = mas.join(' ')
-    
+            mas = params[:arr_a].split.map(&:to_i)
+            index = mas.find_index { |num| num % 3 == 0 }
+            min_value = mas.min
+            mas[mas.index(min_value)] = index
+            @result = mas.join(' ')
           else
-            @result = 'Вы ввели массив без положительных чисел, введите массив с положительынми числами'
+            @result = 'Вы ввели массив без чисел, введите массив с  числами'
           end
         else
          @result = 'Вы не ввели чисел'
